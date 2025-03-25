09988f182775a56937209719f0c2d1e4b16adc04
LougonVitor <vitor.lougon@gmail.com>
2025-03-25 09:55:35 -0300

    feat: updating and setting header components

---
+++ src/components/header/header.tsx
@@ -0,0 +1,14 @@
+import './header.css'
+import './header.js'
+
+function Header() {
+  return (
+    <>
+      <header id='header'>
+        <h1>My Universe!</h1>
+      </header>
+    </>
+  )
+}
+
+export default Header
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Welcome from './pages/welcome'
+import Header from './components/header/header'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Header />
     <Welcome />
   </StrictMode>,
 )
