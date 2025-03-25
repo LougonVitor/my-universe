2ab6bfc36a4bb5843ff249077977291a57f227a8
LougonVitor <vitor.lougon@gmail.com>
2025-03-25 10:20:12 -0300

    feat: finalizing initial changes to header components

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -5,7 +5,19 @@ function Header() {
   return (
     <>
       <header id='header'>
-        <h1>My Universe!</h1>
+        <section id='title-section'>
+          <h1>Vítor <span>Lougon</span></h1>
+        </section>
+        <section id='menu-section'>
+          <nav id='nav-menu'>
+            <ul>
+              <li>Home</li>
+              <li>Projects</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
+        </section>
       </header>
     </>
   )
