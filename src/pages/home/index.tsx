f73b24f203f0ae712eb74ebbbc4a8d3aceb8a04e
Vítor Lougon <vitor.lougon@gmail.com>
2025-11-06 20:59:58 -0300

    feat: inserting the avatar image on the home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,16 +1,20 @@
 import './styles.css'
+import  avatar  from '../../assets/home/avatar-me.png'
 
 function Home() {
   return (
     <>
       <section id='home-container'>
-        <h1>Home</h1>
+        <h1 className='page-title home'>Home</h1>
 
         <div id='inner-container'>
-          <div className='box image'>
-
+          <div className='box-image'>
+            <img src={avatar} alt="" className='avatar-image'/>
           </div>
-          <div className='box info'>
+          <div className='box-info'>
+            
+            <h1>Vítor Lougon</h1>
+            <h2>I'm a Software Enginer</h2>
 
           </div>
         </div>
